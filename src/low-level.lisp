2533caafb823c9e53d1c495f1a7989ffd3ed0365
Kevin Störmer <stoermer@uni-bremen.de>
2018-06-11 17:13:48 +0200

    launchfile angepasst + tf halb richtig

--- src/low-level.lisp
+++ src/low-level.lisp
@@ -29,3 +29,39 @@
 (actionlib:def-exec-callback addition (testValue)
   (roslisp:ros-info (callback) "callback called")
   (actionlib:succeed-current :testResult (+ 1 testValue)))
+
+(defun transformation-Pose-Stamped (pose &optional (endFrame "/base_footprint")) 
+  "transform a Pose-Stamped-msgs with an optional Frame, default is base_footprint" 
+  (roslisp:with-fields 
+      ((startFrame 
+        (STD_msgs-msg:frame_id geometry_msgs-msg:header)) 
+       (x 
+        (geometry_msgs-msg:x geometry_msgs-msg:position geometry_msgs-msg:pose)) 
+       (y 
+        (geometry_msgs-msg:y geometry_msgs-msg:position  geometry_msgs-msg:pose)) 
+       (z 
+        (geometry_msgs-msg:z geometry_msgs-msg:position geometry_msgs-msg:pose))
+       (w
+        (geometry_msgs-msg:w geometry_msgs-msg:orientation geometry_msgs-msg:pose))
+       (xo
+        (geometry_msgs-msg:x geometry_msgs-msg:orientation geometry_msgs-msg:pose))
+       (yo
+        (geometry_msgs-msg:x geometry_msgs-msg:orientation geometry_msgs-msg:pose))
+       (zo
+        (geometry_msgs-msg:z geometry_msgs-msg:orientation geometry_msgs-msg:pose))) 
+      pose 
+    (let 
+        ((transform-listener 
+           (make-instance 'cl-tf:transform-listener)) 
+         (tf-pose-stamped 
+           (cl-tf:make-pose-stamped startFrame 0.0 
+                                    (cl-transforms:make-3d-vector x y z)
+                                    (cl-transforms:make-quaternion xo yo zo w)))) 
+      (catch-Transformation-Pose-Stamped transform-listener tf-pose-stamped endFrame))))
+
+(defun catch-Transformation-Pose-Stamped (transform-listener tf-pose-stamped endFrame)
+  "transform-listener catch transformation (helpfunction)"
+  (sleep 5.0)
+  (cl-tf:transform-pose-stamped transform-listener
+                                :pose tf-pose-stamped
+:target-frame endFrame))
